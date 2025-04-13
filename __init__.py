b66c0e4e0076936d0847dd0e2bb8028dc177f31c
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-04-13 22:38:31 +0100

    change prints to logging

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
 from .router import router
 from .schemas.sponsors_component import SponsorsComponent
 from services.component_registry import ComponentRegistry
+import logging
+
+logger = logging.getLogger("coffeebreak.core")
 
 def register_plugin():
     ComponentRegistry.register_component(SponsorsComponent)
-    print("Sponsors component registered.")
+    logger.debug("Sponsors component registered.")
     return router
 
 def unregister_plugin():
     ComponentRegistry.unregister_component("SponsorsComponent")
-    print("Sponsors component unregistered.")
+    logger.debug("Sponsors component unregistered.")
 
 REGISTER = register_plugin
 UNREGISTER = unregister_plugin
